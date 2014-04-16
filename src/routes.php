a059d09cc3f610921090d9d0496ac315f05a03d8
R.A.Stone <rastone@rastone.net>
2014-04-16 17:55:36 -0400

    Adding API Routes

--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,27 @@
 <?php
 
-Route::get('/bridgebb-auth-api/{username}/{password}', function($username, $password) {
-    //TODO: Auth   
+Route::get('/bridgebb-api/auth/{apikey}/{username}/{password}', function($apikey, $username, $password) {
+    global $app;
+    $bridgeBBApiKey = $app['config']->get('bridgebb::bridgebb-api.apikey');
+    $usernameColumn = $app['config']->get('bridgebb::database.user-model.username-column');
+    $passwordColumn = $app['config']->get('bridgebb::database.user-model.password-column');
+    if ($apikey == $app['config']->get('bridgebb::bridgebb-api.apikey')) {
+        $oUser = $UserModel::where($username_col, $username)
+                ->where($password_col, $password)
+                ->first();
+        return json_encode(array(
+            'response' => 'success',
+            'data' => $oUser
+        ));
+    } else {
+        return json_encode(array(
+            'response' => 'error',
+            'message' => 'Invalid API Key'
+        ));
+    }
+});
+Route::post('/bridgebb-api/register/{apikey}/{userID}/{phpBBID}', function($apikey, $userID, $phpBBID) {
+    global $app;
+    $oUser = $UserModel::find($userID);
+    $oUser->$phpBBIDColumn = $phpBBID;
 });
